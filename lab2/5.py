2feaf2f5c857337938ef88062ff8067af300d17c
befovis <dianacopute@gmail.com>
2024-10-06 21:52:48 +0300

    исправление ошибок

--- lab2/5.py
+++ lab2/5.py
@@ -8,19 +8,14 @@ def count_occurrences(arr, left, right, element):
 def majority_element_rec(arr, left, right):
     if left == right:
         return arr[left]
-
     mid = (left + right) // 2
     left_majority = majority_element_rec(arr, left, mid)
     right_majority = majority_element_rec(arr, mid + 1, right)
-
     if left_majority == right_majority:
         return left_majority
-
     left_count = count_occurrences(arr, left, right, left_majority)
     right_count = count_occurrences(arr, left, right, right_majority)
-
     return left_majority if left_count > right_count else right_majority
-
 def has_majority_element(arr):
     n = len(arr)
     candidate = majority_element_rec(arr, 0, n - 1)
@@ -28,15 +23,11 @@ def has_majority_element(arr):
     if count > n // 2:
         return 1
     return 0
-
-
 if __name__ == "__main__":
     with open("input", "r") as file:
         n = int(file.readline())
         arr = list(map(int, file.readline().split()))
-
     result = has_majority_element(arr)
-
     with open("output", "w") as file:
         file.write(str(result) + "\n")
 tracemalloc.start()
